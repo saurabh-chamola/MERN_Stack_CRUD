8237c2c0e6f1a7a66e768450f1d9b128e09dee75
saurabh-chamola <117541654+saurabh-chamola@users.noreply.github.com>
2026-07-21 13:16:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import UserRoute from "./routes/UserRoute.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 const app = express();
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
